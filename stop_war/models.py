9c86d2f595dc627cfef40caa8bdc4190ef49183f
Denis Ivanenko <ivanenko@ucu.edu.ua>
2022-03-03 21:33:28 +0200

    Fixed model problems

--- stop_war/models.py
+++ stop_war/models.py
@@ -4,4 +4,3 @@ from django.db import models
 class Visit(models.Model):
     user_agent = models.CharField(max_length=100)
     ip = models.CharField(max_length=50)
-    city = models.CharField(max_length=100)
--- stop_war/views.py
+++ stop_war/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
-from django.contrib.gis.utils import GeoIP
 from .models import Visit
 
 
 def home(request):
     ip = get_client_ip(request)
     user_agent = request.META['HTTP_USER_AGENT']
-    city = get_city(request)
-    Visit(ip=ip, city=city, user_agent=user_agent)
-
-    new_visit = Visit(ip=ip, city=city, user_agent=user_agent)
-    new_visit.save()
+    current_visit = Visit(ip=ip,  user_agent=user_agent)
+    current_visit.save()
     counter = Visit.objects.count()
 
     return render(request, 'stop_war/home.html', {'counter': counter})
@@ -24,13 +20,3 @@ def get_client_ip(request):
         ip = request.META.get('REMOTE_ADDR')
     return ip
 
-
-def get_city(request):
-    g = GeoIP()
-    ip = request.META.get('REMOTE_ADDR', None)
-    if ip:
-        city = g.city(ip)['city']
-    else:
-        city = 'Russia'  # default city
-
-    return city

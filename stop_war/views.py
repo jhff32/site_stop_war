d4ce48cf01cd80fbcaeef8a6948903e5d3b2a8c5
jhff32 <66910676+jhff32@users.noreply.github.com>
2022-03-03 22:27:21 +0200

    added unique visitors

--- stop_war/views.py
+++ stop_war/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from .models import Visit
 
+counter = 0
+
 
 def home(request):
+    global counter
     ip = get_client_ip(request)
     user_agent = request.META['HTTP_USER_AGENT']
-    current_visit = Visit(ip=ip,  user_agent=user_agent)
-    current_visit.save()
-    counter = Visit.objects.count()
+    current_visit = Visit(ip=ip, user_agent=user_agent)
+    if not Visit.objects.filter(ip=ip).exists():
+        current_visit.save()
+        counter = Visit.objects.count()
 
     return render(request, 'stop_war/home.html', {'counter': counter})
 
@@ -19,4 +23,3 @@ def get_client_ip(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
-
